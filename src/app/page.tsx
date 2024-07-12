e50cda3182ad596f587b85d962b71c278220052e
Sunil Sapkota <suneelsapkota@gmail.com>
2024-07-12 09:48:16 +0545

    chore: Add lazy loading for images and update landing page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/* PTP verification */}
       <section className="remove-scrollbar container my-auto "> 
         <div className="sub-container max-w-[496px">
           <Image
@@ -13,9 +16,25 @@ export default function Home() {
           alt="Logo"
           className="mb-12 h-10 w-fin"
           />
-          
+          <PatientForm />
+        <div className="text-14-regular mt-20 flex justify-between">
+          <p className="justify-items-end text dark-600 xl:text-left" >
+          © 2024 carePlus
+          </p>
+          <Link href="/?admin=true" className="text-green-500">
+          Admin
+          </Link>
+        </div>
         </div>
       </section>
+      <Image
+      src="/assets/images/onboarding-img.png"
+      height={1000}
+      width={1000}
+      alt="Onboarding Image"
+      className="side-img max-w-[50%]"
+
+      />
     </div>
   );
 }
